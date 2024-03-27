ddb689dc6d80ff68fa621377002355fd0f905c33
Nehareddy1711 <nehaatham@lyrostech.com>
2024-03-27 17:24:48 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { useContext } from "react";
-// import { UserContext } from '../App'
 import { Usecontext1 } from "./UserContext/Usercontext1";
-//import { srikanth } from '../UserContext/Usercontext1';
 import Home from "./Componenta/Home";
 import Login from "./Componenta/Login";
 import Register from "./Componenta/Register";
-
 import Feeds from "./Componenta/Feeds";
 import Posts from "./Componenta/Posts";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
@@ -19,10 +16,7 @@ export default function App() {
     <>
     <Router>
       {flag == 0 || flag == 2 ? <Login /> : <Home />}
-      
       {flag === 2 && <Register />}
-
-      
         <Routes>
           <Route index path={`${PATH}/`}  element={<Feeds />}></Route>
           <Route path={`${PATH}/post`} element={<Posts />}></Route>
--- src/Componenta/Login.js
+++ src/Componenta/Login.js
@@ -1,4 +1,3 @@
-
 import React, { useContext } from 'react';
 import { Usecontext1 } from '../UserContext/Usercontext1';
 import Register from './Register';
